7e430e3f736369a22a881b381bab51847a523d98
Hariprasath <49398090+hariprasath-v@users.noreply.github.com>
2024-10-13 14:46:03 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -7,17 +7,19 @@ import os
 import minsearch
 
 
-# Load JSON data 
-with open("./data/nnet_101_qna_with_id.json", 'rt') as f_in:
-    data = json.load(f_in)
 
+
+# Load data 
+data_vec_url = "./data/llm_answers_mistral_7b_instruct_v0_1_with_vector.csv"
+data = pd.read_csv(data_vec_url)
+data['question_answer_vector']= data['question_answer_vector'].apply(lambda x: [float(i) for i in x.strip("[]").split(",") if i])
 
 
 
 # Create the index
 index = minsearch.Index(
-    text_fields=["question", "answer"],
-    keyword_fields=["tag"]
+    text_fields=["question", "answer_llm"],
+    keyword_fields=["tags"]
 )
 
 # Fit the index with data
@@ -34,9 +36,7 @@ client = OpenAI(
 def search(query):
     results = index.search(
         query=query,
-        num_results=5
-    )
-
+        num_results=5)
     return results
 
 #build prompt
@@ -77,41 +77,80 @@ def rag(query):
     return answer
 
 
+sample_questions = ["What is batch size?",
+                    "How do I choose the number of hidden layers in a neural network?",
+                    "What are the advantages of smaller batch sizes?",
+                    "When is the output of ReLU equal to zero?",
+                    "What is global max pooling?"]
+
+
+st.markdown(
+            """
+        <style>
+            .st-emotion-cache-1c7y2kd {
+                flex-direction: row-reverse;
+                text-align: right;
+            }
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
+
+
 # Initialize conversation history
-if "history" not in st.session_state:
-    st.session_state.history = []
+if "messages" not in st.session_state:
+      st.session_state["messages"] = []
+
+
+
 
 # Streamlit app UI
 st.title("Nnet101_Assistant")
-st.write("Ask me anything related to Nnet basics, and I'll do my best to respond!")
+st.write("Ask me about neural network basics, and I'll do my best to respond!")
 
-# Text input
-user_input = st.text_input("Your Question:", "")
 
-# Process input and get response
-if user_input:
-    # Add user input to conversation history
-    st.session_state.history.append({"role": "user", "content": user_input})
 
-    # Display the response
-    bot_response = rag(user_input)
-    st.session_state.history.append(
-        {"role": "assistant", "content": bot_response})
-    st.write(f"**Assistant**: {bot_response}")
+user_input = st.chat_input("Message Nnet101")
+# Chat UI using chat_input and chat_message
+if user_input: 
 
-# Display conversation history
-st.write("### Conversation History")
-for message in st.session_state.history:
+    # Add user message to chat history
+    
+    st.session_state.messages.append({"role": "user", "content": user_input})
+
+    # Process input and get response (assuming rag() is defined)
+    bot_response = rag(user_input)  # Call the RAG model for response generation
+
+    
+
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": bot_response})
+ 
+
+
+
+
+# Sidebar for chat controls
+with st.sidebar:
+    st.header("Chat Controls")
+
+    with st.popover("Sample Questions"):
+        for q in sample_questions:
+            st.markdown(q)
+    
+    # Button to clear conversation history
+    if st.button("Clear Conversation"):
+        st.session_state["messages"].clear()  # Clear message history
+
+    # Message if history is empty
+    if not st.session_state.messages:
+        st.write("No conversation history.")
+
+# Display conversation history from session state
+for message in st.session_state.messages:
     if message["role"] == "user":
-        st.write(f"**You**: {message['content']}")
+        with st.chat_message("user"):
+            st.markdown(message["content"])
     else:
-        st.write(f"**Assistant**: {message['content']}")
-
-# Clear history button
-if st.button("Clear Conversation"):
-    st.session_state.history.clear()  # Clear history using the clear method
-    st.write("Conversation history cleared!")  # Optionally show a message
-
-# Optionally, re-display conversation history
-if not st.session_state.history:  # If history is empty, show a message
-    st.write("No conversation history.")
+        with st.chat_message("assistant"):
+            st.markdown(message["content"]) 
